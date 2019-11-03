20ce6756771c8039009beb7e764ad135123716c1
JavaStroyTeller <FlameXander@rambler.ru>
2019-11-03 22:26:26 +0300

    My super m25 feature added

--- src/main/java/com/flamexander/demo/MainApp.java
+++ src/main/java/com/flamexander/demo/MainApp.java
@@ -3,5 +3,6 @@ package com.flamexander.demo;
 public class MainApp {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Super M025 Feature");
     }
 }
