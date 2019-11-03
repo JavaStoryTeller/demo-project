2c44158b69a2b3ce7dc7b2365bb8a6da8fb06dbd
JavaStroyTeller <FlameXander@rambler.ru>
2019-11-03 22:32:52 +0300

    m25 part 2

--- src/main/java/com/flamexander/demo/MainApp.java
+++ src/main/java/com/flamexander/demo/MainApp.java
@@ -4,5 +4,6 @@ public class MainApp {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Super M025 Feature");
+        System.out.println("Super M025 Feature Part 2");
     }
 }
